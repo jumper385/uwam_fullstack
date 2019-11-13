e3a4136837d477f8a4ad1ac89e14809f8a4cbc7a
jumper385 <henrychen385@gmail.com>
2019-11-13 18:09:06 +0800

    added mongodb connect

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const mongoose = require('mongoose')
-const express = require('express')
+const app = require('express')()
 const bodyParser = require('body-parser')
+
+const PORT = process.env.PORT || 8080
+const MONGO_URL = process.env.MONGO_URI || `mongodb+srv://nova:st18chenh@cluster0-ztrfz.azure.mongodb.net/test?retryWrites=true&w=majority`
+
+mongoose.connect(
+    `${MONGO_URL}/uwam_articles`, 
+    {useUnifiedTopology:true, useNewUrlParser:true},
+    () => console.log(`joined mongo db ${MONGO_URL}`)
+)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/')
+
+app.listen(PORT, () => console.log(`listening to port ${PORT}`))
