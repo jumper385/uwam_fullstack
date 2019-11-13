d3eb9d18b52554ddd2149090da5a0dccbb375b94
jumper385 <henrychen385@gmail.com>
2019-11-13 20:31:35 +0800

    ummm.... a lil hotfix?

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ mongoose.connect(
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static('client/build'))
 
 if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
     app.get('*', (req,res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
