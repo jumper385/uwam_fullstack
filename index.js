9628712cdf19d0b1a20b4f1fd5bbf4f2c409b6c8
jumper385 <henrychen385@gmail.com>
2019-11-24 23:31:29 +0800

    we can finally pipe images!!!

--- index.js
+++ index.js
@@ -5,16 +5,34 @@ const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 const bodyParser = require('body-parser')
+const GridFsStorage = require('multer-gridfs-storage')
+const Grid = require('gridfs-stream')
+const multer = require('multer')
+const shortid = require('shortid')
+
 const Schemas = require('./Schemas/Schemas')
 
 const PORT = process.env.PORT || 8080
 const MONGO_URL = process.env.MONGO_URI || `mongodb+srv://nova:st18chenh@cluster0-ztrfz.azure.mongodb.net/test?retryWrites=true&w=majority`
 
-mongoose.connect(
-    `${MONGO_URL}`, 
-    {useUnifiedTopology:true, useNewUrlParser:true},
-    () => console.log(`joined mongo db ${MONGO_URL}`)
+const conn = mongoose.createConnection(
+    `${MONGO_URL}`,
+    {useUnifiedTopology:true, useNewUrlParser:true}
 )
+
+let gfs = null
+conn.once('open', () => {
+    gfs = Grid(conn.db, mongoose.mongo)
+    gfs.collection('fs')
+})
+
+// const imgConn = mongoose.connect(imgURL, {useUnifiedTopology:true, useNewUrlParser:true})
+
+const storage = new GridFsStorage({ 
+    url: MONGO_URL,
+    file: (req,file) => ({filename:shortid.generate()})
+})
+const upload = multer({ storage })
 
 app.use((req,res,next) => {
     res.header('Access-Control-Allow-Origin', '*')
@@ -32,13 +50,6 @@ app.use('/', express.static(path.join(__dirname, 'client/build')))
 app.get('/client*', (req,res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
-
-// if (process.env.NODE_ENV === 'production'){
-//     app.use(express.static('client/build'))
-//     app.get('*', (req,res) => {
-//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
 
 app.route('/api/users')
     .get(async (req,res) => {
@@ -76,6 +87,24 @@ app.route('/api/articles/:id')
         console.log(req.params.id)
         const Article = await Schemas.Article.findOne({_id:req.params.id})
         res.json(Article)
+    })
+
+app.post('/api/images', upload.single('photo'), async(req,res,next) => {
+    res.json(req.file)
+})
+
+app.get('/api/images', async(req,res) => {
+    const imageList = await gfs.files.find().toArray()
+    res.json(imageList)
+})
+
+app.route('/api/images/:id')
+    .get((req,res) => {
+        gfs.files.findOne({filename:req.params.id}, (err,file) => {
+            if(err) throw err
+            const readstream = gfs.createReadStream(file.filename)
+            readstream.pipe(res)
+        })
     })
 
 app.listen(PORT, () => console.log(`listening to port ${PORT}`))
